44d1169f686b1b1881893d9661fd87b5950be1ed
JahajjainanIRV <72501214+JahajjainanIRV@users.noreply.github.com>
2023-04-02 13:15:39 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-// run `node index.js` in the terminal
+var http = require('http');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const server = http.createServer(function (req, res) {
+  res.setHeader('Content-Type0', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Page Title</title></head>');
+  res.write(
+    '<body><h1>This is a Heading</h1><p>This is a paragraph.</p></body>'
+  );
+  res.end('</html>');
+});
+server.listen(3000);
